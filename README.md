bed871e47d3b29b53425c836b83cb6f9e2473a00
Lexyth <Lexyth@users.noreply.github.com>
2024-07-19 20:11:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Tailwind + TypeScript + Vite + ESLint
+
+A template for npm packages with React, Tailwind, TypeScript, Vite, and ESLint already set up.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="m-4 text-3xl font-bold underline">
+        A template for npm packages with React, Tailwind, TypeScript, Vite, and
+        ESLint already set up.
+      </h1>
+    </>
+  );
+}
+
+export default App;
