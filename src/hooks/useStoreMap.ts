f6ad83f8963c988d097424e0b9e32844f778867a
Lexyth <marfy@web.de>
2024-07-19 22:09:06 +0200

    feat: Add useStoreMap hook

--- src/hooks/useStoreMap.ts
+++ src/hooks/useStoreMap.ts
@@ -1,3 +1,5 @@
+import { createStore, useStore } from "./useStore";
+
 import type { Store } from "./useStore";
 
 export type StoreMap<T> = Map<string, Store<T>>;
@@ -8,3 +10,19 @@ export function createStoreMap<T>(
   return new Map(initialStores);
 }
 
+export function useStoreMap<T>(
+  storeMap: ReturnType<typeof createStoreMap<T>>,
+  key: string,
+  initialValue: Parameters<typeof createStore<T>>[0]
+): ReturnType<typeof useStore<T>> {
+  const store =
+    storeMap.get(key) ??
+    (() => {
+      const s = createStore<T>(initialValue);
+      storeMap.set(key, s);
+      return s;
+    })();
+
+  const state = useStore(store);
+  return state;
+}
